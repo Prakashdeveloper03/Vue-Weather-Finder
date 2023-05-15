82723a16f1cc3c5505b253e11fdde4c6abdb4144
Siva Prakash <prakashdeveloper@outlook.in>
2023-05-15 13:16:30 +0530

    Added Tailwindcss & Updated Vue file

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,110 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+    <main class="min-h-screen py-25 bg-gradient-to-b">
+      <br />
+      <div class="w-full flex justify-center items-center h-full">
+        <input
+          type="text"
+          class="block w-1/2 py-3 px-3 text-lg text-gray-800 rounded-full shadow-md bg-opacity-50 hover:bg-opacity-75 focus:outline-none focus:bg-opacity-75"
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+      <br />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div v-if="typeof weather.main != 'undefined'">
+        <div class="text-center">
+          <div class="text-white text-2xl font-medium text-center text-shadow-lg">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
+          <div class="text-white text-lg font-italic text-center">{{ dateBuilder() }}</div>
+        </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+        <div class="text-center">
+          <div
+            class="inline-block py-10 px-25 text-7xl font-bold text-white text-shadow-lg bg-opacity-25 rounded-full shadow-md"
+          >
+            {{ Math.round(weather.main.temp) }}°c
+          </div>
+          <div class="text-white text-4xl font-bold italic text-shadow-lg">
+            {{ weather.weather[0].main }}
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+body {
+  font-family: 'montserrat', sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app.warm {
+  background-image: url('./assets/warm-bg.jpg');
 }
 </style>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'app',
+  data() {
+    return {
+      api_key: 'YOUR_API_KEY',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {} as Record<string, any>
+    }
+  },
+  methods: {
+    async fetchWeather(e: KeyboardEvent) {
+      if (e.key === 'Enter') {
+        try {
+          const res = await fetch(
+            `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+          )
+          const data = await res.json()
+          this.weather = data
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    },
+    dateBuilder() {
+      const d = new Date()
+      const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+      ]
+      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+      const day = days[d.getDay()]
+      const date = d.getDate()
+      const month = months[d.getMonth()]
+      const year = d.getFullYear()
+
+      return `${day} ${date} ${month} ${year}`
+    }
+  }
+})
+</script>
